207422574168938ce2f3dcdc96519991e397e7f9
EmilSatyev <satyev.emil@gmail.com>
2022-10-12 20:47:43 +0500

    second

--- src/App.js
+++ src/App.js
@@ -1,9 +1,147 @@
-import './App.css';
+import "./App.scss";
+import image1 from "./assets/image/slider.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <h1>emil</h1>
+    <div className="trello-master">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src={image1} alt="image" />
+              Design and Research
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>
+              <img src={image1} alt="image" />
+              Design and Research
+            </li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src={image1} alt="image" />
+              Design and Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src={image1} alt="image" />
+              Design and Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src={image1} alt="image" />
+              Design and Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src={image1} alt="image" />
+              Design and Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src={image1} alt="image" />
+              Design and Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src={image1} alt="image" />
+              Design and Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src={image1} alt="image" />
+              Design and Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src={image1} alt="image" />
+              Design and Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src={image1} alt="image" />
+              Design and Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src={image1} alt="image" />
+              Design and Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
